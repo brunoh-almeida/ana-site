99db2a652aa80d072d187dd0d384cb5d3ce48e3c
Bruno Almeida <bruno.almeida@thoughtworks.com>
2024-11-25 15:14:29 -0300

    fixing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/assets/images/*.jpg', '**/assets/fonts/*.ttf']
 })
