297a80f41372d16c6fe7e25c70257b35f7fb9243
Bruno Almeida <bruno.almeida@thoughtworks.com>
2025-02-15 09:29:11 -0300

    creating a dockerfile and docker compose file

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
+  server: {
+    port: 8080,
+    strictPort:true,
+    host: true,
+    origin: "http://0.0.0.0:8080"
+  },
   plugins: [react()],
   assetsInclude: ['**/assets/images/*.jpg', '**/assets/fonts/*.ttf']
 })
